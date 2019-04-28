8adc7d5f0c598388c96695df48899f431bc522d3
EvanFManuella@gmail.com <evan.manuella@fdmgroup.com>
2019-04-28 13:00:34 -0400

    MVP get and POST

--- Controllers/HashBucketController.cs
+++ Controllers/HashBucketController.cs
@@ -20,6 +20,7 @@ namespace HashBucket.Controllers
         public HashBucketController(HashBucketContext context)
         {
             _context = context;
+            _context.hashValues.Add(new EncryptedKeyValue() { HashKey = "sanity", EncryptedValue = "check" });
         }
 
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -33,17 +34,22 @@ namespace HashBucket.Controllers
         public ActionResult<string> Get(string hashKey)
         {
             log.Warn("Request body for key: " + hashKey);
-            var body = _context.hashValues.Find(hashKey).EncryptedValue;
-
+            var body = _context.hashValues.Find(hashKey);
             // your key is invalid, you get indistinguishable junk
             // TODO, we shouldn't user random for security
+            // TODO this should be consistent per key, so that 
+            // all responses are consistent
+
+            // Right now I can tell if a key is valid if it returns
+            // the same thing twice in a row
+
             if (body == null)
             {
                 var random = new Random();
                 return new string(Enumerable.Repeat(chars, 100)
                   .Select(s => s[random.Next(s.Length)]).ToArray());
             }
-            return body;
+            return body.EncryptedValue;
         }
     
 
@@ -52,10 +58,12 @@ namespace HashBucket.Controllers
          *  Takes the stores the EncryptedValue in the DB with key HashKey
         ***/
         [HttpPost]
-        public void Post([FromBody] EncryptedKeyValue value)
+        public IActionResult Post([FromBody] EncryptedKeyValue value)
         {
             log.Warn("Received key: " + value.HashKey + " and value: " + value.EncryptedValue);
-            _context.hashValues.Add(value);
+            var x = _context.hashValues.Add(new EncryptedKeyValue() { HashKey = value.HashKey, EncryptedValue = value.EncryptedValue });
+            _context.SaveChanges();
+            return new OkResult();
         }
 
     }
